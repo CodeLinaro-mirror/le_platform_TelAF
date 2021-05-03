4c3a47c057e6cef3e9af4fd333cc4305ab5566f4
Debjit Roy <debjroy@codeaurora.org>
2021-05-03 18:41:50 +0530

    telaf: Export file_contexts path for selinux
    file_contexts file path exported in makefile.
    To be used by legato build to write selinux attrs to squashfs image
    
    Change-Id: Ic6b5846fa8cf24cdd70535ada55397112b21fc2b

--- Makefile
+++ Makefile
@@ -9,11 +9,14 @@ export TELAF_ROOT := $(CURDIR)
 export TELAF_BUILD := $(CURDIR)/build
 export LEGATO_ROOT := $(CURDIR)/../legato/legato-af
 export LEGATO_BUILD := $(CURDIR)/../legato/legato-af/build
+export GEN_FILE_CONTEXTS := $(CURDIR)/security/selinux/tools/generate_telaf_file_contexts.sh
+export SELINUX_FILE_CONTEXTS := ${CURDIR}/security/selinux/sepolicy/files/file_contexts
 
 $(TARGETS):
 ifneq ($(TELAF_BUILD), $(wildcard $(TELAF_BUILD)))
 	@ln -sf $(LEGATO_BUILD) $(TELAF_BUILD)
 endif
+	$(shell $(GEN_FILE_CONTEXTS))
 	$(MAKE) -C $(LEGATO_ROOT) $@ TELAF_ROOT_SET=$(TELAF_ROOT)
 
 $(UTILITIES):
---
+++ security/selinux/tools/generate_telaf_file_contexts.sh
@@ -0,0 +1,66 @@
+# Copyright (c) 2021 The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#!/bin/bash
+BASEDIR=$(dirname $(realpath $0))
+
+SEPOLICY_DIR=$(dirname ${BASEDIR})/sepolicy
+FILE_CONTEXTS_DIR=${SEPOLICY_DIR}/files
+FILE_CONTEXTS_FILE=${FILE_CONTEXTS_DIR}/file_contexts
+
+FC_FILES=$(find ${SEPOLICY_DIR} -type f -name "*.fc")
+if [ ! -z "$FC_FILES" ]
+then
+    if [ ! -d ${FILE_CONTEXTS_DIR} ]
+    then
+        mkdir ${FILE_CONTEXTS_DIR}
+    fi
+    if [ -e ${FILE_CONTEXTS_FILE} ]
+    then
+        rm ${FILE_CONTEXTS_FILE}
+    fi
+    touch ${FILE_CONTEXTS_FILE}
+fi
+
+for FC_FILE in ${FC_FILES}
+do
+    while read file rule; do
+        if [[ ${file:0:7} == "/legato" ]]
+        then
+            #echo "file: $file rule: $rule ";
+            rule=${rule//gen_context(/};
+            rule=${rule//t,s/t:s};
+            rule=${rule//)/};
+            if [ -z "${file:7}" ]
+            then
+                echo "/    $rule" >> $FILE_CONTEXTS_FILE;
+            else
+                echo "${file:7}    $rule" >> $FILE_CONTEXTS_FILE;
+            fi
+        fi
+    done < $FC_FILE
+done

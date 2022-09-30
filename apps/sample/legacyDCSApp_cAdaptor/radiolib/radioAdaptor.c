bffe0ed3f4582ab1adb9587a57c4e69fb0187b61
Demi Xu <quic_chunxu@quicinc.com>
2022-09-30 12:18:21 +0800

    tafDataSvc: Sample apps of using data service
    - Build data service as a library
    - Build radio service as the second library
    - Legacy apps build with the service libraries
    
    Change-Id: Ie3d84ef3a2d30cf23565555c27bb442f54b5d7c3

---
+++ apps/sample/legacyDCSApp_cAdaptor/radiolib/radioAdaptor.c
@@ -0,0 +1,90 @@
+/*
+* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the
+* disclaimer below) provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above
+* copyright notice, this list of conditions and the following
+* disclaimer in the documentation and/or other materials provided
+* with the distribution.
+*
+* * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "radioAdaptor.h"
+
+
+/**
+ * Initialize a legato thread and connect with taf_radio service.
+ */
+void radioAdaptor_Connect(pthread_once_t *legatoThreadOnceKey)
+{
+    pthread_once(legatoThreadOnceKey, legatoContextInitialization);
+    taf_radio_ConnectService();
+}
+
+/**
+ * Enter the event loop of TelAF.
+ */
+void radioAdaptor_RegisterEventLoop(void)
+{
+    // Enter the event loop to make sure telaf events can be handled properly
+    le_event_RunLoop();
+}
+
+/**
+ * Check whether the radio power status is on.
+ */
+bool radioAdaptor_IsRadioPowerOn(void){
+    bool ret = false;
+    le_result_t res;
+    le_onoff_t state;
+
+    res = taf_radio_GetRadioPower(&state, RADIO_DEFAULT_PHONE_ID);
+
+    if (res != LE_OK)
+    {
+        return ret;
+    }
+
+    switch (state)
+    {
+        case LE_OFF:
+            LE_INFO("Power OFF");
+            break;
+        case LE_ON:
+            LE_INFO("Power ON");
+            ret = true;
+            break;
+        default:
+            ret = false;
+    }
+
+    return ret;
+}
+
+static void legatoContextInitialization(){
+    // Set the TelAF thread context for connection with radio service
+    le_thread_InitLegatoThreadData("telaf_radio_thread");
+}
---
+++ apps/sample/legacyDCSApp_cppAdaptor/README.md
@@ -0,0 +1,42 @@
+# A sample application of TelAF Data Call Service in C++
+The application calls taf_dcs APIs and taf_radio APIs via a dynamic library called DataAdaptor and RadioAdaptor.
+It dumps the available data profiles and makes a data call with the default profile when the radio power status is on.
+## Get Started
+Before building the application, the toolchain needs to be installed properly and TelAF framework needs to be built successfully.
+
+### Step 1 Source the tool chain
+```bash
+source /opt/qct/sa515m/environment-setup-armv7at2hf-neon-oe-linux-gnueabi
+```
+
+### Step 2 Setup TelAF building environment
+```bash
+cd ~/telaf
+./bin/legs
+export TARGET=sa515m
+```
+
+### Step 3 Build the application
+```bash
+cd ~/legacyDCSApp_cppAdaptor
+mkdir build
+cd build
+cmake ..
+make
+```
+
+The output libdataAdaptor.so locates in build/datalib/, libreadioAdaptor.so locates in build/radiolib,
+the executable DataAppDemo locates in build/.
+
+### Step 4 Run the application on the target
+Push libdataAdaptor.so and libreadioAdaptor.so to /data/lib on device, DataAppDemo to /data/ on device via ADB.
+On device, run the following commands to set external library path and bind the APIs to taf_dcs and taf_radio service.
+
+Lastly run the executable file.
+```bash
+export LD_LIBRARY_PATH=/data/lib/
+sdir bind "<root>.taf_dcs" "<root>.taf_dcs"
+sdir bind "<root>.taf_radio" "<root>.taf_radio"
+./DataAppDemo
+```
+

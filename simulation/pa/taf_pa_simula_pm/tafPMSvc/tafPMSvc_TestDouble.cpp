45d6d2877ea491c985a1af9eedc0231ff732bd35
Yang Zheng <quic_yangzhen@quicinc.com>
2023-10-10 21:40:22 +0800

    simulation: Add a tafPMSvc test double to resolve dependencies
    Change-Id: Ief4651637f24d0f65fc89ce7bb7e0e84b7119c08

---
+++ simulation/pa/taf_pa_simula_pm/tafPMSvc/tafPMSvc_TestDouble.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * @file       tafPMSvc_TestDouble.cpp
+ * @brief      Just a Test Double for Power Manager Service
+ */
+
+#include "legato.h"
+#include "interfaces.h"
+
+taf_pm_StateChangeHandlerRef_t taf_pm_AddStateChangeHandler
+(taf_pm_StateChangeHandlerFunc_t handlerPtr, void* contextPtr)
+{
+    LE_INFO("[Simulation TestDouble]: %s", __FUNCTION__);
+    return NULL;
+}
+
+void taf_pm_RemoveStateChangeHandler(taf_pm_StateChangeHandlerRef_t handlerRef)
+{
+    LE_INFO("[Simulation TestDouble]: %s", __FUNCTION__);
+}
+
+COMPONENT_INIT
+{
+    LE_INFO("[Simulation TestDouble]: Power Manager Service started");
+}

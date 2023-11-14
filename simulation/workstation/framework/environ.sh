2464b24eb91ed1878de7d03bc3dec5a7d5eda915
Yang Zheng <quic_yangzhen@quicinc.com>
2023-11-14 13:31:07 +0800

    simulation: Support multiple containers phase 1
    Now support two containers, master and slave
    
    Change-Id: Ibc686837373912118d90fb9d15b91f71e41c25e9

--- simulation/workstation/framework/environ.sh
+++ simulation/workstation/framework/environ.sh
@@ -9,3 +9,51 @@ SDK_BIN_HOME=PATH=/legato/systems/current/sdk_rootfs/bin
 export PATH=$SDK_BIN_HOME:$SCRIPT_DIR:$PATH
 
 alias simula="python3 $SCRIPT_DIR/main.py"
+
+# Example: slave [list | to <address> | shell (empty)]
+function simula_slave ()
+{
+    case "$1" in
+    "help")
+        python3 $SCRIPT_DIR/main.py slave --help
+        ;;
+    "list")
+        python3 $SCRIPT_DIR/main.py slave --list
+        ;;
+    "to")
+        python3 $SCRIPT_DIR/main.py slave --to $2
+        ;;
+    "shell" | "")
+        python3 $SCRIPT_DIR/main.py slave --shell
+        ;;
+    *)
+        echo "Unknown sub-command [$1], please check help page"
+        ;;
+    esac
+}
+
+alias slave="simula_slave"
+
+# rslave <remote-commands>
+alias rslave="python3 $SCRIPT_DIR/main.py slave --remote -- "
+
+function simula_master ()
+{
+    case "$1" in
+    "help")
+        python3 $SCRIPT_DIR/main.py master --help
+        ;;
+    "info")
+        python3 $SCRIPT_DIR/main.py master --info
+        ;;
+    "shell" | "")
+        python3 $SCRIPT_DIR/main.py master --shell
+        ;;
+    *)
+        echo "Unknown sub-command [$1], please check help page"
+        ;;
+    esac
+}
+
+# master [info | shell (empty)]
+alias master="simula_master"

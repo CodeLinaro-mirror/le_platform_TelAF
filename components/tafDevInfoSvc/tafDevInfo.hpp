8a48515343735344a74924fd657bdacd99a50b1b
Anuj Joshi <quic_anujjosh@quicinc.com>
2024-01-09 15:47:28 +0530

    tafDevInfoSvc: Get IMEI information of the device
    Change-Id: I7b3df80501a6a2cdf03090982b1fb2aa4f4fdeaa

---
+++ components/tafDevInfoSvc/tafDevInfo.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+/*
+ * @file        tafDevInfo.hpp
+ * @brief       Interface for device information service object.
+                The functions in this file are impletmented internally.
+ */
+
+#ifndef TAFDEVINFO_HPP
+#define TAFDEVINFO_HPP
+
+#include "interfaces.h"
+#include "legato.h"
+#include "tafSvcIF.hpp"
+#include <future>
+#include <telux/platform/PlatformFactory.hpp>
+
+using namespace telux::common;
+using namespace telux::tafsvc;
+using namespace telux::platform;
+
+namespace telux {
+namespace tafsvc {
+
+    class tafdevinfoServiceStatusListener : public telux::platform::IDeviceInfoListener {
+    public:
+        void onServiceStatusChange(telux::common::ServiceStatus serviceStatus) override;
+    };
+
+    class taf_info : public ITafSvc {
+    public:
+        taf_info() = default;
+        ~taf_info() = default;
+
+        static taf_info& GetInstance();
+        void Init();
+
+        le_result_t GetIMEI(char* imeiPtr, size_t numElements);
+
+        std::shared_ptr<telux::platform::IDeviceInfoListener> devinfoServiceStatusListener
+            = nullptr;
+        std::shared_ptr<IDeviceInfoManager> deviceInfoManager = nullptr;
+    };
+}
+}
+
+#endif

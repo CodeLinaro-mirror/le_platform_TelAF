6010df82059acd9abffe24aebf19b76e09325598
Yang Zheng <quic_yangzhen@quicinc.com>
2024-01-23 21:06:18 +0800

    simulation: Fix the issues of system library dependencies and precheck
    Change-Id: I434181d68bd90fafc38592e1f4572facb80d6bb8

--- simulation/scripts/forward_to_container.sh
+++ simulation/scripts/forward_to_container.sh
@@ -3,12 +3,15 @@
 # Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-current_dir=$(dirname "$0")
-project_top=$current_dir/../../../
+# TELAF_DEV_IN_CONTAINER is from container environement args
+
+# current_dir=$(dirname "$0")
+project_top="$TELAF_DEV_IN_CONTAINER"
 telaf_root=$project_top/telaf
 
 cd $telaf_root
 echo -e "\n-- container information record begin --"
 cat /etc/os-release
+echo -e "\nTelAF-Simulation Project : ${project_top}\n"
 echo -e "-- container information record done --\n"
 eval "$@"

3c10f440dde4d6af39dfea55631040644888ea73
Yang Zheng <quic_yangzhen@quicinc.com>
2024-01-26 15:04:10 +0800

    simulation: Support Ubuntu 22.04
    Change-Id: Iad6d0286b4a94f9ac625b604547df41df11dd707

--- simulation/deps/dependence.mk
+++ simulation/deps/dependence.mk
@@ -61,7 +61,7 @@ _post_deps:
 #-> [compile]  3. Compile the dependent project
 #-> [install]  4. Install all generated stuff to SIMULATION_DEPS_ROOTFS or SIMULATION_HOST_XTOOLS
 
-.PHONY: boost
+.PHONY: _boost simula-boost boost_
 
 BOOST_URL?=https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.gz
 _BOOST_VERSION=$(notdir $(lastword $(subst /, ,$(BOOST_URL))))
@@ -79,10 +79,10 @@ simula-boost: boost_
 boost_:
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_BOOST_VERSION) $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_BOOST_VERSION) $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(_BOOST_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(BOOST_URL)]" \
-	  && wget -q -O $(SIMULATION_DEPS_SOURCE)/$(_BOOST_VERSION) $(BOOST_URL) > /dev/null
+	  && wget -O $(SIMULATION_DEPS_SOURCE)/$(_BOOST_VERSION) $(BOOST_URL) > $(SIMULATION_DEPS_SOURCE)/__download_$(_BOOST_VERSION).log 2>&1
 	$Q echo "[$@] extract to [$(SIMULATION_DEPS_SOURCE)/$@]" \
 	  && cd $(SIMULATION_DEPS_SOURCE) \
 	  && tar xfz $(_BOOST_VERSION) && mv $(BOOST_VERSION) $@
@@ -99,11 +99,12 @@ boost_:
 	$Q echo "[$@] Done"
 
 
-.PHONE: cmake
+.PHONY: _cmake simula-cmake cmake_
 
-CMAKE_URL?=https://cmake.org/files/v3.15/cmake-3.15.3.tar.gz
+# NOTE: cmake >= 3.23.xx is required for finding OpenSSL3 in /lib64 suffix
+CMAKE_URL?=https://cmake.org/files/v3.23/cmake-3.23.0-linux-x86_64.sh
 _CMAKE_VERSION=$(notdir $(lastword $(subst /, ,$(CMAKE_URL))))
-CMAKE_VERSION=$(_CMAKE_VERSION:%.tar.gz=%)
+CMAKE_VERSION=$(_CMAKE_VERSION:%-Linux-x86_64.sh=%)
 
 # $(error $(shell ls -l $(SIMULATION_HOST_XTOOLS)/bin/cmake))
 
@@ -117,27 +118,23 @@ simula-cmake: cmake_
 cmake_:
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_CMAKE_VERSION) $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_CMAKE_VERSION) $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(_CMAKE_VERSION).log
 #-> 2. [download]
-	$Q echo "[$@] downloading from [$(BOOST_URL)]" \
-	  && wget -q -O $(SIMULATION_DEPS_SOURCE)/$(_CMAKE_VERSION) $(CMAKE_URL) > /dev/null
-	$Q echo "[$@] extract to [$(SIMULATION_DEPS_SOURCE)/$@]" \
-	  && cd $(SIMULATION_DEPS_SOURCE) \
-	  && tar xfz $(_CMAKE_VERSION) && mv $(CMAKE_VERSION) $@
+	$Q echo "[$@] downloading from [$(CMAKE_URL)] to [$@]" \
+	  && wget -O $(SIMULATION_DEPS_SOURCE)/$(_CMAKE_VERSION) $(CMAKE_URL) > $(SIMULATION_DEPS_SOURCE)/__download_$(_CMAKE_VERSION).log 2>&1
+	$Q echo "[$@] setting up [$(SIMULATION_DEPS_SOURCE)/$@]" \
+	  && cd $(SIMULATION_DEPS_SOURCE) && mkdir -p $(SIMULATION_DEPS_SOURCE)/$@ && mv $(_CMAKE_VERSION) $(SIMULATION_DEPS_SOURCE)/$@ \
+	  && chmod a+x $(SIMULATION_DEPS_SOURCE)/$@/$(_CMAKE_VERSION)
 #-> 3. [compile]
-	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
-	  && echo "[$@] configure firstly" \
-	  && ./configure --prefix=$(SIMULATION_HOST_XTOOLS) > ./__config.log 2>&1 \
-	  && echo "[$@] compiling ..."  \
-	  && make -j $(shell nproc) > ./__build.log 2>&1
+	$Q echo "[$@] configuration & compilation are not needed"
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	  && echo "[$@] installing ..." \
-	  && make install > ./__install.log 2>&1
+	  && ./$(_CMAKE_VERSION) --prefix=$(SIMULATION_HOST_XTOOLS) --skip-license > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
-.PHONE: vsomeip
+.PHONY: _vsomeip simula-vsomeip vsomeip_
 
 # VSOMEIP_URL?=https://github.com/COVESA/vsomeip/archive/refs/tags/3.1.20.3.tar.gz
 # NOTE: cmake >= 3.13.xx is required for vsomeip 3.4.9-r1
@@ -155,10 +152,10 @@ simula-vsomeip: vsomeip_
 vsomeip_: _cmake _boost
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_VSOMEIP_VERSION) $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$(_VSOMEIP_VERSION) $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(_VSOMEIP_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(VSOMEIP_URL)]" \
-	  && wget -q -O $(SIMULATION_DEPS_SOURCE)/$(_VSOMEIP_VERSION) $(VSOMEIP_URL) > /dev/null
+	  && wget -O $(SIMULATION_DEPS_SOURCE)/$(_VSOMEIP_VERSION) $(VSOMEIP_URL) > $(SIMULATION_DEPS_SOURCE)/__download_$(_VSOMEIP_VERSION).log 2>&1
 	$Q echo "[$@] extract to [$(SIMULATION_DEPS_SOURCE)/$@]" \
 	  && cd $(SIMULATION_DEPS_SOURCE) \
 	  && tar xfz $(_VSOMEIP_VERSION) && mv $(VSOMEIP_VERSION) $@
@@ -170,17 +167,17 @@ vsomeip_: _cmake _boost
 	      -DBoost_INCLUDE_DIR=$(SIMULATION_DEPS_ROOTFS)/include \
 	      -DBoost_LIBRARY_DIR=$(SIMULATION_DEPS_ROOTFS)/lib \
 	      -DENABLE_SIGNAL_HANDLING=1 \
-	      -DCMAKE_INSTALL_PREFIX=$(SIMULATION_DEPS_ROOTFS) .. > ./__config.log 2>&1 \
+	      -DCMAKE_INSTALL_PREFIX=$(SIMULATION_DEPS_ROOTFS) -DCMAKE_PREFIX_PATH=$(SIMULATION_DEPS_ROOTFS) .. > ./__config.log 2>&1 \
 	  && echo "[$@] compiling ..." \
-	    && make > ./__build.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . -- -j $(shell nproc) > ./__build.log 2>&1
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@/build \
 	  && echo "[$@] installing ..." \
-	    && make install > ./__install.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . --target install > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
-.PHONE: openssl
+.PHONY: _openssl simula-openssl openssl_
 
 OPENSSL_URL?=https://github.com/openssl/openssl.git
 OPENSSL_VERSION=openssl-3.0.9
@@ -195,11 +192,11 @@ simula-openssl: openssl_
 openssl_:
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(OPENSSL_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(OPENSSL_URL)]" \
 	  && git clone -q --depth 1 --branch ${OPENSSL_VERSION} --single-branch \
-	         ${OPENSSL_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download.log 2>&1
+	         ${OPENSSL_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download_$(OPENSSL_VERSION).log 2>&1
 	$Q echo "[$@] just from git repo, no need to extract"
 #-> 3. [compile]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
@@ -210,11 +207,11 @@ openssl_:
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	  && echo "[$@] installing ..." \
-	    && make install_sw > ./__install.log 2>&1
+	    && make -j $(shell nproc) install_sw > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
-.PHONE: curl
+.PHONY: _curl simula-curl curl_
 
 CURL_URL?=https://github.com/curl/curl.git
 CURL_VERSION=curl-7_69_1
@@ -226,30 +223,30 @@ $(SIMULATION_DEPS_ROOTFS)/include/curl/curl.h:
 	$Q $(MAKE) --no-print-directory simula-curl
 
 simula-curl: curl_
-curl_:
+curl_: _cmake _openssl
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(CURL_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(CURL_URL)]" \
 	  && git clone -q --depth 1 --branch ${CURL_VERSION} --single-branch \
-	         ${CURL_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download.log 2>&1
+	         ${CURL_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download_$(CURL_VERSION).log 2>&1
 	$Q echo "[$@] just from git repo, no need to extract"
 #-> 3. [compile]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	  && echo "[$@] configure firstly" \
 	    && mkdir -p build && cd build \
-	    && cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} .. > ./__config.log 2>&1 \
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} -DCMAKE_PREFIX_PATH=$(SIMULATION_DEPS_ROOTFS) .. > ./__config.log 2>&1 \
 	  && echo "[$@] compiling ..." \
-	    && make > ./__build.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . -- -j $(shell nproc) > ./__build.log 2>&1
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@/build \
 	  && echo "[$@] installing ..." \
-	    && make install > ./__install.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . --target install > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
-.PHONE: capi_core_rt_
+.PHONY: _capi_core_rt simula-capi-core-rt capi_core_rt_
 
 CAPI_CORE_RT_URL?=https://git.codelinaro.org/clo/la/platform/external/capicxx-core-runtime.git
 CAPI_CORE_RT_VERSION=capi_core_rt_3.2.0
@@ -261,14 +258,14 @@ $(SIMULATION_DEPS_ROOTFS)/lib/libCommonAPI.so:
 	$Q $(MAKE) --no-print-directory simula-capi-core-rt
 
 simula-capi-core-rt: capi_core_rt_
-capi_core_rt_:
+capi_core_rt_: _cmake
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(CAPI_CORE_RT_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(CAPI_CORE_RT_URL)]" \
 	  && git clone -q --branch github/master \
-	         ${CAPI_CORE_RT_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download.log 2>&1 \
+	         ${CAPI_CORE_RT_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download_$(CAPI_CORE_RT_VERSION).log 2>&1 \
 	  && cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	    && git checkout 89720d3c63bbd22cbccc80cdc92c2f2dd20193ba > /dev/null 2>&1
 	$Q echo "[$@] just from git repo, no need to extract"
@@ -277,18 +274,18 @@ capi_core_rt_:
 	  && echo "[$@] configure firstly" \
 	    && sed -i 's/-Werror=extra-semi//g' CMakeLists.txt \
 	    && mkdir -p build && cd build \
-	    && cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} \
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} \
 	             -DCMAKE_PREFIX_PATH=${SIMULATION_DEPS_ROOTFS} .. > ./__config.log 2>&1 \
 	  && echo "[$@] compiling ..." \
-	    && make > ./__build.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . -- -j $(shell nproc) > ./__build.log 2>&1
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@/build \
 	  && echo "[$@] installing ..." \
-	    && make install > ./__install.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . --target install > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
-.PHONE: capi_someip_rt_
+.PHONY: _capi_someip_rt simula-capi-someip-rt capi_someip_rt_
 
 CAPI_SOMEIP_RT_URL?=https://git.codelinaro.org/clo/la/platform/external/capicxx-someip-runtime.git
 CAPI_SOMEIP_RT_VERSION=capi_someip_rt_3.2.0
@@ -300,14 +297,14 @@ $(SIMULATION_DEPS_ROOTFS)/lib/libCommonAPI-SomeIP.so:
 	$Q $(MAKE) --no-print-directory simula-capi-someip-rt
 
 simula-capi-someip-rt: capi_someip_rt_
-capi_someip_rt_:
+capi_someip_rt_: _cmake
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_$(CAPI_SOMEIP_RT_VERSION).log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(CAPI_SOMEIP_RT_URL)]" \
 	  && git clone -q --branch github/master \
-	         ${CAPI_SOMEIP_RT_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download.log 2>&1 \
+	         ${CAPI_SOMEIP_RT_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download_$(CAPI_SOMEIP_RT_VERSION).log 2>&1 \
 	  && cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	    && git checkout 0ad2bdc1807fc0f078b9f9368a47ff2f3366ed13 > /dev/null 2>&1
 	$Q echo "[$@] just from git repo, no need to extract"
@@ -315,14 +312,14 @@ capi_someip_rt_:
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	  && echo "[$@] configure firstly" \
 	    && mkdir -p build && cd build \
-	    && cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} \
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake -DCMAKE_INSTALL_PREFIX=${SIMULATION_DEPS_ROOTFS} \
 	             -DCMAKE_PREFIX_PATH=${SIMULATION_DEPS_ROOTFS} .. > ./__config.log 2>&1 \
 	  && echo "[$@] compiling ..." \
-	    && make > ./__build.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . -- -j $(shell nproc) > ./__build.log 2>&1
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@/build \
 	  && echo "[$@] installing ..." \
-	    && make install > ./__install.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . --target install > ./__install.log 2>&1
 	$Q echo "[$@] Done"
 
 
@@ -333,6 +330,8 @@ CAPI_CORE_GENERATOR_VERSION=capi_core_gen_3.2.0.1
 CAPI_SOMEIP_GENERATOR_VERSION=capi_someip_gen_3.2.0.1
 
 CAPI_CORE_TOOLS_URL?=https://github.com/GENIVI/capicxx-core-tools.git
+
+.PHONY: _capi_tools simula-capi-core-generator simula-capi-someip-generator simula-capi-core-tools capi_core_tools_
 
 _capi_tools: $(SIMULATION_HOST_XTOOLS)/bin/commonapi_core_generator/commonapi-core-generator-linux-x86_64 \
              $(SIMULATION_HOST_XTOOLS)/bin/commonapi_someip_generator/commonapi-someip-generator-linux-x86_64 \
@@ -351,7 +350,7 @@ $(SIMULATION_DEPS_ROOTFS)/bin/E01HelloWorldService:
 simula-capi-core-generator:
 	$Q echo "[capi_core_generator_] downloading binary tools from [$(CAPI_CORE_GENERATOR_URL)]" \
 	  && cd $(SIMULATION_DEPS_SOURCE) \
-	  && wget -q -O $(SIMULATION_DEPS_SOURCE)/$(CAPI_CORE_GENERATOR_VERSION).zip $(CAPI_CORE_GENERATOR_URL) > /dev/null \
+	  && wget -O $(SIMULATION_DEPS_SOURCE)/$(CAPI_CORE_GENERATOR_VERSION).zip $(CAPI_CORE_GENERATOR_URL) > ./__download_$(CAPI_CORE_GENERATOR_VERSION).log 2>&1 \
 	&& echo "[capi_core_generator_] extract zip files" \
 	  && mkdir -p $(SIMULATION_HOST_XTOOLS)/bin/commonapi_core_generator \
 	  && unzip $(SIMULATION_DEPS_SOURCE)/$(CAPI_CORE_GENERATOR_VERSION).zip -d $(SIMULATION_HOST_XTOOLS)/bin/commonapi_core_generator > /dev/null
@@ -359,20 +358,20 @@ simula-capi-core-generator:
 simula-capi-someip-generator:
 	$Q echo "[capi_someip_generator_] downloading binary tools from [$(CAPI_SOMEIP_GENERATOR_URL)]" \
 	  && cd $(SIMULATION_DEPS_SOURCE) \
-	  && wget -q -O $(SIMULATION_DEPS_SOURCE)/$(CAPI_SOMEIP_GENERATOR_VERSION).zip $(CAPI_SOMEIP_GENERATOR_URL) > /dev/null \
+	  && wget -O $(SIMULATION_DEPS_SOURCE)/$(CAPI_SOMEIP_GENERATOR_VERSION).zip $(CAPI_SOMEIP_GENERATOR_URL) > ./__download_$(CAPI_SOMEIP_GENERATOR_VERSION).log 2>&1 \
 	&& echo "[capi_someip_generator_] extract zip files" \
 	  && mkdir -p $(SIMULATION_HOST_XTOOLS)/bin/commonapi_someip_generator \
 	  && unzip $(SIMULATION_DEPS_SOURCE)/$(CAPI_SOMEIP_GENERATOR_VERSION).zip -d $(SIMULATION_HOST_XTOOLS)/bin/commonapi_someip_generator > /dev/null
 
 simula-capi-core-tools: capi_core_tools_
-capi_core_tools_:
+capi_core_tools_: _cmake
 #-> 1. [clean]
 	$Q echo "[$@] cleaning compression and directories" \
-	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@
+	  && rm -rf $(SIMULATION_DEPS_SOURCE)/$@ $(SIMULATION_DEPS_SOURCE)/__download_capicxx-core-tools.log
 #-> 2. [download]
 	$Q echo "[$@] downloading from [$(CAPI_CORE_TOOLS_URL)]" \
 	  && git clone -q --branch master \
-	         ${CAPI_CORE_TOOLS_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download.log 2>&1 \
+	         ${CAPI_CORE_TOOLS_URL} $(SIMULATION_DEPS_SOURCE)/$@ > $(SIMULATION_DEPS_SOURCE)/__download_capicxx-core-tools.log 2>&1 \
 	  && cd $(SIMULATION_DEPS_SOURCE)/$@ \
 	    && git checkout 5ed80df56a64fdd9bf22b2f240a608e77be8262c > /dev/null 2>&1
 	$Q echo "[$@] just from git repo, no need to extract"
@@ -389,12 +388,12 @@ capi_core_tools_:
 	    && commonapi-core-generator-linux-x86_64 -sk ./fidl/E01HelloWorld.fidl -d ./src-gen/core > __generate_idl_core.log 2>&1 \
 	    && commonapi-someip-generator-linux-x86_64 ./fidl/E01HelloWorld-SomeIP.fdepl -d ./src-gen/someip > __generate_idl_someip.log 2>&1 \
 	  && mkdir -p build && cd build \
-	    && cmake -DCMAKE_PREFIX_PATH=${SIMULATION_DEPS_ROOTFS} \
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake -DCMAKE_PREFIX_PATH=${SIMULATION_DEPS_ROOTFS} \
 	             -DINSTALL_DIR=${SIMULATION_DEPS_ROOTFS} .. > ./__config.log 2>&1 \
 	  && echo "[$@] compiling ..." \
-	    && make > ./__build.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . -- -j $(shell nproc) > ./__build.log 2>&1
 #-> 4. [install]
 	$Q cd $(SIMULATION_DEPS_SOURCE)/$@/CommonAPI-Examples/E01HelloWorld/build \
 	  && echo "[$@] installing ..." \
-	    && make install > ./__install.log 2>&1
+	    && $(SIMULATION_HOST_XTOOLS)/bin/cmake --build . --target install > ./__install.log 2>&1
 	$Q echo "[$@] Done"

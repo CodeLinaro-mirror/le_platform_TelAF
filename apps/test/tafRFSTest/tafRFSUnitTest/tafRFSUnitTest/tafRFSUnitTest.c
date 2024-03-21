5014a2af5b6cefe541944448a8ed9e045e10a996
Chen Stark <quic_yuhachen@quicinc.com>
2024-03-21 16:54:09 +0800

    tafRFSLib: Initiate Robustness File System component
    Change-Id: I808df4ed9bcbea7273e64ad883d6d8e87b3cbf29
    Signed-off-by: Chen Stark <quic_yuhachen@quicinc.com>

---
+++ apps/test/tafRFSTest/tafRFSUnitTest/tafRFSUnitTest/tafRFSUnitTest.c
@@ -0,0 +1,291 @@
+/*
+ *  Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+/*
+ * @file       tafRFSUnitTest.c
+ * @brief      This file includes test functions for tafRFSLib component
+ */
+
+#include "legato.h"
+#include "interfaces.h"
+#include "linux/file.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tafRFSLib.h"
+
+#define TEST_FILE_PATH "/data/testRFS_File.txt"
+#define TEST_DATA_SIZE (1024 * 10)
+#define TIMEOUT_ITEM_TEST 5
+
+#define OP_READ    "read"
+#define OP_WRITE   "write"
+#define OP_DELETE  "delete"
+#define OP_CORRUPT "corrupt"
+
+static le_mem_PoolRef_t TestRequestPool;
+static le_thread_Ref_t TestThreadRef;
+static le_sem_Ref_t sem_TestItem;
+
+typedef enum
+{
+    READ,
+    WRITE,
+    DELETE,
+    CORRUPT
+}
+TestOperation_t;
+
+typedef struct
+{
+    TestOperation_t op;
+}
+TestRequest_t;
+
+TestOperation_t testSequence[] = {WRITE, CORRUPT, READ, DELETE};
+//--------------------------------------------------------------------------------------------------
+/**
+ * Set semaphore timeout
+ */
+//--------------------------------------------------------------------------------------------------
+
+__attribute__((unused)) le_result_t WaitForSem_Timeout
+(
+    le_sem_Ref_t semRef,
+    uint32_t seconds
+)
+{
+    le_clk_Time_t timeToWait = {seconds, 0};
+
+    return le_sem_WaitWithTimeOut(semRef, timeToWait);
+}
+
+// Main function of low-priority background thread.
+__attribute__((unused)) static void* TestThread
+(
+    void* contextPtr // not used.
+)
+{
+    le_event_RunLoop();
+}
+
+__attribute__((unused)) static void ErrCallback(taf_rfs_Error_t error, const char* filePath)
+{
+    LE_ERROR("error %d happened with file %s", error, filePath);
+}
+
+__attribute__((unused)) static char* GenerateTestData()
+{
+    char* data = malloc(TEST_DATA_SIZE + 1);
+    LE_TEST_ASSERT(data != NULL, "Allocate test data");
+    for(size_t i = 0; i < TEST_DATA_SIZE; ++i)
+    {
+        data[i] = 'A' + (i % 26); // Use cycling chars for filling data
+    }
+    data[TEST_DATA_SIZE] = '\0';
+    return data;
+}
+
+__attribute__((unused)) static void Test_Init()
+{
+    le_result_t res = taf_rfs_Init(true, ErrCallback);
+
+    LE_TEST_ASSERT(res == LE_OK, "Test taf_rfs_Init");
+}
+
+__attribute__((unused)) static void Test_Write()
+{
+    char* testData = GenerateTestData();
+    LE_TEST_ASSERT(testData != NULL, "Allocate test data for write");
+
+    int fd = taf_rfs_Open(TEST_FILE_PATH, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    LE_TEST_ASSERT(fd >= 0, "Test taf_rfs_Open for write");
+
+    ssize_t bytesWritten = taf_rfs_Write(fd, (uint8_t*)testData, TEST_DATA_SIZE);
+    LE_TEST_ASSERT(bytesWritten == TEST_DATA_SIZE, "Test taf_rfs_Write for write");
+
+    LE_TEST_ASSERT(taf_rfs_Close(fd) == 0, "Test taf_rfs_Close for write");
+
+    free(testData);
+}
+
+__attribute__((unused)) static void Test_Read()
+{
+    char* buffer = malloc(TEST_DATA_SIZE + 1);
+    LE_TEST_ASSERT(buffer != NULL, "Allocate buffer for read");
+    buffer[TEST_DATA_SIZE] = '\0';
+
+    int fd = taf_rfs_Open(TEST_FILE_PATH, O_RDONLY, 0);
+    LE_TEST_ASSERT(fd >= 0, "Test taf_rfs_Open for read");
+
+    size_t expectedSize = TEST_DATA_SIZE;
+    ssize_t bytesRead = taf_rfs_Read(fd, (uint8_t*)buffer, &expectedSize);
+    LE_TEST_ASSERT(bytesRead == TEST_DATA_SIZE, "Test taf_rfs_Read for read");
+
+    LE_TEST_ASSERT(taf_rfs_Close(fd) == 0, "Test taf_rfs_Close for read");
+
+    char* data = malloc(TEST_DATA_SIZE + 1);
+    LE_TEST_ASSERT(data != NULL, "Allocate test data");
+    for(size_t i = 0; i < TEST_DATA_SIZE; ++i)
+    {
+        data[i] = 'A' + (i % 26); // Use cycling chars for filling data
+    }
+
+    LE_TEST_ASSERT(memcmp(buffer, data, TEST_DATA_SIZE) == 0, "Test file integrity");
+
+    free(buffer);
+}
+
+__attribute__((unused)) static void Test_Delete()
+{
+    taf_rfs_Delete(TEST_FILE_PATH);
+    struct stat st;
+    LE_TEST_ASSERT(stat(TEST_FILE_PATH, &st) == -1, "Test taf_rfs_Delete");
+}
+
+// This function simulates corrupting file
+__attribute__((unused)) static void Simulate_Corrupt() 
+{
+    // set up random element
+    srand((unsigned) time(NULL));
+
+    FILE* file = fopen(TEST_FILE_PATH, "r+b");
+    if (file == NULL)
+    {
+        perror("Failed to open file");
+        return;
+    }
+
+    // move to a random position in the file
+    fseek(file, rand() % TEST_DATA_SIZE, SEEK_SET);
+
+    // generate random data to overwrite original data
+    for (int i = 0; i < 10; i++)
+    {
+        char randomByte = rand() % 256;
+        fwrite(&randomByte, sizeof(char), 1, file);
+        if (ferror(file))
+        {
+            perror("Failed to write to file");
+            break;
+        }
+    }
+
+    printf("File corruption simulation completed.\n");
+    fclose(file);
+}
+
+static void ProcessTest
+(
+    void* param1Ptr, // request object pointer
+    void* param2Ptr  // not used
+)
+{
+    TestRequest_t* requestPtr = (TestRequest_t*)param1Ptr;
+
+    switch(requestPtr->op)
+    {
+        case READ:
+        Test_Read();
+        break;
+        case WRITE:
+        Test_Write();
+        break;
+        case DELETE:
+        Test_Delete();
+        break;
+        case CORRUPT:
+        Simulate_Corrupt();
+        break;
+    }
+
+    le_mem_Release(requestPtr);
+
+    le_sem_Post(sem_TestItem);
+}
+
+/**
+ * Start app : app start tafRFSUnitTest
+ * Execute app : app runProc tafRFSUnitTest tafRFSUnitTest -- <operation>
+ * e.g. app runProc tafRFSUnitTest tafRFSUnitTest -- write
+ *
+ * If no <operation> is input, test app will run write, corrupt, read and delete functions in sequence.
+ */
+COMPONENT_INIT
+{
+    LE_TEST_PLAN(LE_TEST_NO_PLAN);
+
+    LE_TEST_INFO("=== telaf RFS test BEGIN ===");
+
+    TestRequestPool = le_mem_CreatePool("Test Request", sizeof(TestRequest_t));
+
+    sem_TestItem = le_sem_Create("TestItemSem", 0);
+
+    TestThreadRef = le_thread_Create("Background Test Thread",
+                                        TestThread,
+                                        NULL);
+
+    le_thread_Start(TestThreadRef);
+
+    LE_TEST_INFO("=== Test provision ===");
+    Test_Init();
+
+    if (le_arg_NumArgs() > 0)
+    {
+        TestRequest_t* requestPtr = (TestRequest_t*)le_mem_ForceAlloc(TestRequestPool);
+
+        const char* operation = le_arg_GetArg(0);
+        if (NULL == operation)
+        {
+            LE_ERROR("operation is NULL");
+            exit(EXIT_FAILURE);
+        }
+
+        if (strcmp(operation, OP_READ) == 0)
+        {
+            requestPtr->op = READ;
+        }
+        else if (strcmp(operation, OP_WRITE) == 0)
+        {
+            requestPtr->op = WRITE;
+        }
+        else if (strcmp(operation, OP_DELETE) == 0)
+        {
+            requestPtr->op = DELETE;
+        }
+        else if (strcmp(operation, OP_CORRUPT) == 0)
+        {
+            requestPtr->op = CORRUPT;
+        }
+        else
+        {
+            LE_ERROR("Invalid operation");
+        }
+
+        le_event_QueueFunctionToThread(TestThreadRef, ProcessTest, requestPtr, NULL);
+
+        LE_ASSERT_OK(WaitForSem_Timeout(sem_TestItem, TIMEOUT_ITEM_TEST));
+    }
+    else
+    {
+        LE_INFO("Starting file function tests with parameterized data size...");
+
+        for(uint i = 0; i < (sizeof(testSequence)/sizeof(testSequence[0])); i++)
+        {
+            TestRequest_t* requestPtr = (TestRequest_t*)le_mem_ForceAlloc(TestRequestPool);
+            requestPtr->op = testSequence[i];
+            le_event_QueueFunctionToThread(TestThreadRef, ProcessTest, requestPtr, NULL);
+
+            LE_ASSERT_OK(WaitForSem_Timeout(sem_TestItem, TIMEOUT_ITEM_TEST));
+        }
+
+        LE_INFO("All tests passed with data size: %d", TEST_DATA_SIZE);
+    }
+
+    LE_TEST_EXIT;
+
+    LE_TEST_INFO("=== telaf RFS test END ===");
+}
+
---
+++ components/tafRFSLib/tafRFSLib.cpp
@@ -0,0 +1,649 @@
+/*
+ *  Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include "tafRFSLib.h"
+
+#include "legato.h"
+#include "limit.h"
+
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <selinux/selinux.h>
+#include <sys/xattr.h>
+#include <string.h>
+#include <openssl/sha.h>
+#include <openssl/md5.h>
+#include <openssl/evp.h>
+
+// RFS file backup storage
+#define RFS_STORAGE "/persist/rfs/"
+#define RFS_BACKUP_STORAGE RFS_STORAGE"backup/"
+#define RFS_MAX_BACKUP_FILENAME 1024
+#define RFS_FILE_EXTENDED_ATTR_MD5 "security.md5"
+
+// backup configuration
+static bool enableBackup = false;
+static char appBackupStorage[LIMIT_MAX_PATH_BYTES] = {0};
+static taf_rfs_ErrorHandler_t errCB = nullptr;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Event ID for RFS events
+ */
+//--------------------------------------------------------------------------------------------------
+static le_event_Id_t ErrorEventId;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Error message structure.
+ */
+//--------------------------------------------------------------------------------------------------
+typedef struct
+{
+    taf_rfs_Error_t error;
+    char filePath[LIMIT_MAX_PATH_BYTES];
+}
+RFS_ErrorMsg_t;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Calculates MD5 for a specified file.
+ */
+//--------------------------------------------------------------------------------------------------
+static void CalculateFileMD5(const char* filePath, char* md5Str, size_t md5StrSize)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    if (md5StrSize < (MD5_DIGEST_LENGTH * 2) + 1)
+    {
+        LE_ERROR("Output buffer is too small for MD5 hash.\n");
+        return;
+    }
+
+    FILE* file = fopen(filePath, "rb");
+    if (!file) {
+        perror("Failed to open file");
+        return;
+    }
+
+    EVP_MD_CTX* mdCtx = EVP_MD_CTX_new();
+    const EVP_MD* md = EVP_md5();
+    unsigned char mdValue[EVP_MAX_MD_SIZE];
+    unsigned int mdLen, i;
+
+    if (!EVP_DigestInit_ex(mdCtx, md, NULL))
+    {
+        LE_ERROR("Digest initialization failed.\n");
+        fclose(file);
+        EVP_MD_CTX_free(mdCtx);
+        return;
+    }
+
+    // Read the file and update the digest
+    unsigned char buffer[1024];
+    size_t bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0)
+    {
+        if (!EVP_DigestUpdate(mdCtx, buffer, bytesRead)) {
+            LE_ERROR("Digest update failed.\n");
+            fclose(file);
+            EVP_MD_CTX_free(mdCtx);
+            return;
+        }
+    }
+
+    if (!EVP_DigestFinal_ex(mdCtx, mdValue, &mdLen))
+    {
+        LE_ERROR("Digest finalization failed.\n");
+        fclose(file);
+        EVP_MD_CTX_free(mdCtx);
+        return;
+    }
+
+    // Convert the hash to a hex string
+    for (i = 0; i < mdLen; i++)
+    {
+        snprintf(&(md5Str[i*2]), 3, "%02x", mdValue[i]);
+    }
+
+    fclose(file);
+    EVP_MD_CTX_free(mdCtx);
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Sets file MD5 to its file extended attribute
+ */
+//--------------------------------------------------------------------------------------------------
+static le_result_t SetFileMD5ToExtendedAttr(const char* filePath)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    char md5Str[(MD5_DIGEST_LENGTH * 2) + 1];
+    CalculateFileMD5(filePath, md5Str, sizeof(md5Str));
+
+    // write MD5 to the file extension attribute
+    if (setxattr(filePath, RFS_FILE_EXTENDED_ATTR_MD5, md5Str, strlen(md5Str), 0) < 0)
+    {
+        LE_ERROR("Failed to set MD5 attribute: %s", strerror(errno));
+
+        RFS_ErrorMsg_t errMsg;
+        errMsg.error = RFS_ERR_SET_HASH;
+        snprintf(errMsg.filePath, sizeof(errMsg.filePath), "%s", filePath);
+        le_event_Report(ErrorEventId, (void*)&errMsg, sizeof(RFS_ErrorMsg_t));
+
+        return LE_FAULT;
+    }
+    return LE_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Calculates file path SHA1
+ */
+//--------------------------------------------------------------------------------------------------
+static void CalculateFilePathSHA1(const char* filePath, char* outputHash)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    SHA1((unsigned char*)filePath, strlen(filePath), hash);
+    // transfer SHA1 to a hex string
+    for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+    {
+        snprintf(outputHash + (i * 2), sizeof(outputHash), "%02x", hash[i]);
+    }
+    outputHash[SHA_DIGEST_LENGTH * 2] = '\0';
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Checks if the backup is already in app storage
+ */
+//--------------------------------------------------------------------------------------------------
+static bool IsBackupInAppStorage(const char* filePath)
+{
+    if(strlen(appBackupStorage) > 0)
+    {
+        char sha1Hash[SHA_DIGEST_LENGTH * 2 + 1];
+        CalculateFilePathSHA1(filePath, sha1Hash);
+
+        char backupPath[RFS_MAX_BACKUP_FILENAME];
+        snprintf(backupPath, sizeof(backupPath), "%s%s", appBackupStorage, sha1Hash);
+
+        struct stat st;
+        if (stat(backupPath, &st) == 0)
+        {
+            LE_INFO("%s backup is in app storage", filePath);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Restores file from backup storage
+ */
+//--------------------------------------------------------------------------------------------------
+static le_result_t ReplaceFileWithBackup(const char* filePath)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    char sha1Hash[SHA_DIGEST_LENGTH * 2 + 1];
+    CalculateFilePathSHA1(filePath, sha1Hash);
+
+    char backupPath[RFS_MAX_BACKUP_FILENAME];
+
+    if(IsBackupInAppStorage(filePath))
+    {
+        snprintf(backupPath, sizeof(backupPath), "%s%s", appBackupStorage, sha1Hash);
+    }
+    else
+    {
+        snprintf(backupPath, sizeof(backupPath), "%s%s", RFS_BACKUP_STORAGE, sha1Hash);
+    }
+
+    LE_INFO("replace %s with %s", filePath, backupPath);
+
+    int inputFd = open(backupPath, O_RDONLY);
+    if (inputFd == -1)
+    {
+        LE_ERROR("Failed to open backup file");
+        return LE_FAULT;
+    }
+
+    struct stat stat_buf;
+    if (fstat(inputFd, &stat_buf) == -1)
+    {
+        LE_ERROR("Failed to get backup file size");
+        close(inputFd);
+        return LE_FAULT;
+    }
+
+    int outputFd = open(filePath, O_WRONLY | O_CREAT | O_TRUNC, stat_buf.st_mode);
+    if (outputFd == -1)
+    {
+        LE_ERROR("Failed to open original file for writing");
+        close(inputFd);
+        return LE_FAULT;
+    }
+
+    off_t offset = 0;
+    ssize_t result = sendfile(outputFd, inputFd, &offset, stat_buf.st_size);
+    if (result == -1)
+    {
+        LE_ERROR("Failed to replace original file with backup");
+        close(inputFd);
+        close(outputFd);
+        return LE_FAULT;
+    }
+
+#ifdef LE_CONFIG_ENABLE_SELINUX
+    char* selinuxContext = NULL;
+
+    // obtain and set selinux context
+    if (getfilecon(backupPath, &selinuxContext) >= 0)
+    {
+        if (setfilecon(filePath, selinuxContext) < 0)
+        {
+            LE_ERROR("Failed to set SELinux context on the backup file");
+        }
+        freecon(selinuxContext);
+    }
+#endif
+
+    close(inputFd);
+    close(outputFd);
+
+    SetFileMD5ToExtendedAttr(filePath);
+    return LE_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Backs up file and selinux context
+ */
+//--------------------------------------------------------------------------------------------------
+static le_result_t BackUpFileAndSELinuxContext(const char* sourcePath, const char* targetPath)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    LE_INFO("sourcePath: %s, targetPath: %s", sourcePath, targetPath);
+    int inputFd, outputFd;
+    struct stat stat_buf;
+    off_t offset = 0;
+    ssize_t sent;
+    char* selinuxContext = NULL;
+
+    inputFd = open(sourcePath, O_RDONLY);
+    if (inputFd < 0)
+    {
+        LE_ERROR("Failed to open source file for copying");
+        return LE_FAULT;
+    }
+
+    if (fstat(inputFd, &stat_buf) < 0)
+    {
+        LE_ERROR("Failed to get file size for copying");
+        close(inputFd);
+        return LE_FAULT;
+    }
+
+    outputFd = open(targetPath, O_WRONLY | O_CREAT | O_TRUNC, stat_buf.st_mode);
+    if (outputFd < 0)
+    {
+        LE_ERROR("Failed to open target file for copying");
+        close(inputFd);
+        return LE_FAULT;
+    }
+
+    sent = sendfile(outputFd, inputFd, &offset, stat_buf.st_size);
+    if (sent < 0)
+    {
+        LE_ERROR("Failed to copy file");
+        close(inputFd);
+        close(outputFd);
+        return LE_FAULT;
+    }
+#ifdef LE_CONFIG_ENABLE_SELINUX
+    // obtain and set selinux context
+    if (getfilecon(sourcePath, &selinuxContext) >= 0)
+    {
+        if (setfilecon(targetPath, selinuxContext) < 0)
+        {
+            LE_ERROR("Failed to set SELinux context on the backup file");
+        }
+        freecon(selinuxContext);
+    }
+#endif
+    close(inputFd);
+    close(outputFd);
+    return LE_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Backs up file to storage
+ */
+//--------------------------------------------------------------------------------------------------
+static le_result_t BackupFileToStorage
+(
+    const char* filePath,
+    const char* backupDir
+)
+{
+    LE_INFO("----- %s -----", __FUNCTION__);
+    // Calculate SHA1 as the backup file name
+    char sha1Hash[SHA_DIGEST_LENGTH * 2 + 1];
+    CalculateFilePathSHA1(filePath, sha1Hash);
+    char backupPath[RFS_MAX_BACKUP_FILENAME];
+    snprintf(backupPath, sizeof(backupPath), "%s%s", backupDir, sha1Hash);
+
+    // back up file
+    if (BackUpFileAndSELinuxContext(filePath, backupPath) != LE_OK)
+    {
+        // trigger error event
+        LE_ERROR("Failed to backup file");
+
+        RFS_ErrorMsg_t errMsg;
+        errMsg.error = RFS_ERR_BACKUP;
+        snprintf(errMsg.filePath, sizeof(errMsg.filePath), "%s", filePath);
+
+        le_event_Report(ErrorEventId, (void*)&errMsg, sizeof(RFS_ErrorMsg_t));
+    }
+    LE_INFO("----- %s finished -----", __FUNCTION__);
+    return LE_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Deletes backup file from storage
+ */
+//--------------------------------------------------------------------------------------------------
+static void DeleteBackup
+(
+    const char* filePath
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    char sha1Hash[SHA_DIGEST_LENGTH * 2 + 1];
+    CalculateFilePathSHA1(filePath, sha1Hash);
+    char backupPath[RFS_MAX_BACKUP_FILENAME];
+
+    if(IsBackupInAppStorage(filePath))
+    {
+        snprintf(backupPath, sizeof(backupPath), "%s%s", appBackupStorage, sha1Hash);
+    }
+    else
+    {
+        snprintf(backupPath, sizeof(backupPath), "%s%s", RFS_BACKUP_STORAGE, sha1Hash);
+    }
+
+    LE_INFO("delete backup: %s", backupPath);
+    unlink(backupPath);
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Handler to process a error handler.
+ */
+//--------------------------------------------------------------------------------------------------
+static void ProcessErrorHandler
+(
+    void* context
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    RFS_ErrorMsg_t* ErrMsg = (RFS_ErrorMsg_t*)context;
+
+    LE_ERROR("Error %d happened for %s", ErrMsg->error, ErrMsg->filePath);
+
+    if(errCB != nullptr)
+    {
+        errCB(ErrMsg->error, ErrMsg->filePath);
+    }
+}
+
+extern "C" LE_SHARED le_result_t taf_rfs_Init
+(
+    bool backup,
+    taf_rfs_ErrorHandler_t handlerFunc
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+    LE_INFO("backup = %d", backup);
+
+    enableBackup = backup;
+
+    if(enableBackup == false)
+    {
+        return LE_OK;
+    }
+
+    if(handlerFunc != nullptr)
+    {
+        LE_INFO("Set up error handler");
+        errCB = handlerFunc;
+    }
+       return LE_OK;
+}
+
+extern "C" LE_SHARED int taf_rfs_Open
+(
+    const char *filePathPtr,
+    int flags,
+    mode_t mode
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+    LE_INFO("filePathPtr: %s", filePathPtr);
+
+    if(enableBackup == false)
+    {
+        return open(filePathPtr, flags, mode);
+    }
+
+    struct stat st;
+    if (stat(filePathPtr, &st) == 0)
+    {
+        // file exists
+
+        char storedMd5Str[(MD5_DIGEST_LENGTH * 2) + 1] = {0};
+        ssize_t len = getxattr(filePathPtr, RFS_FILE_EXTENDED_ATTR_MD5, storedMd5Str, sizeof(storedMd5Str));
+
+        if (len > 0)
+        {
+            char md5Str[(MD5_DIGEST_LENGTH * 2) + 1] = {0};
+            CalculateFileMD5(filePathPtr, md5Str, sizeof(md5Str));
+
+            if (strncmp(storedMd5Str, md5Str, MD5_DIGEST_LENGTH * 2) != 0)
+            {
+                // MD5 is not matched, restore the file from backup storage
+                if (ReplaceFileWithBackup(filePathPtr) != 0)
+                {
+                    LE_ERROR("Failed to replace file with backup");
+                    return -1;
+                }
+            }
+        }
+        else
+        {
+            LE_ERROR("Failed to get MD5 from extended attribute: %s", strerror(errno));
+
+            RFS_ErrorMsg_t errMsg;
+            errMsg.error = RFS_ERR_GET_HASH;
+            snprintf(errMsg.filePath, sizeof(errMsg.filePath), "%s", filePathPtr);
+            le_event_Report(ErrorEventId, (void*)&errMsg, sizeof(RFS_ErrorMsg_t));
+        }
+    }
+    else if (!(flags & O_CREAT))
+    {
+        LE_ERROR("File does not exist and O_CREAT not specified");
+        return -1;
+    }
+
+    int fd = open(filePathPtr, flags, mode);
+    return fd;
+}
+
+extern "C" LE_SHARED int taf_rfs_Close
+(
+    int fd
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+
+    if(enableBackup == false)
+    {
+        return close(fd);
+    }
+
+    // use fcntl and F_GETFL to get flags of the fd
+    int flags = fcntl(fd, F_GETFL);
+    if (flags != -1)
+    {
+        // check if the fd has write permission
+        if (!(flags & O_WRONLY) && !(flags & O_RDWR))
+        {
+            LE_INFO("The fd does not have write permission, close it");
+            return close(fd);
+        }
+    }
+
+    LE_INFO("The fd has write permission.");
+
+    // Need to finish and return this function ASAP, push off uneccessary tasks to event process
+
+    char filePath[LIMIT_MAX_PATH_BYTES];
+
+    // construt the path to /proc/[pid]/fd/[fd]
+    snprintf(filePath, sizeof(filePath), "/proc/self/fd/%d", fd);
+
+    // get the actual path from the fd
+    char actualPath[LIMIT_MAX_PATH_BYTES];
+    ssize_t len = readlink(filePath, actualPath, sizeof(actualPath)-1);
+    if (len != -1)
+    {
+        actualPath[len] = '\0'; // ensure the string end with null terminator
+        LE_INFO("The file path is: %s\n", actualPath);
+    }
+
+    SetFileMD5ToExtendedAttr(actualPath);
+
+    BackupFileToStorage(actualPath, RFS_BACKUP_STORAGE);
+
+    return close(fd);
+}
+
+extern "C" LE_SHARED int taf_rfs_Read
+(
+    int fd,
+    uint8_t* bufPtr,
+    size_t* sizePtr
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+    return read(fd, bufPtr, *sizePtr);
+}
+
+extern "C" LE_SHARED int taf_rfs_Write
+(
+    int fd,
+    const uint8_t* bufPtr,
+    size_t sizePtr
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+    return write(fd, bufPtr, sizePtr);
+}
+
+extern "C" LE_SHARED void taf_rfs_Delete
+(
+    const char* filePathPtr
+)
+{
+    LE_INFO("%s", __FUNCTION__);
+    LE_INFO("filePathPtr: %s", filePathPtr);
+
+    unlink(filePathPtr);
+
+    if(enableBackup == true)
+    {
+        DeleteBackup(filePathPtr);
+    }
+}
+
+COMPONENT_INIT
+{
+    // initializing
+
+    LE_INFO("COMPONENT_INIT - tafRFSLib");
+
+    //Create the storage for backup files
+    struct stat sb;
+
+    // assume RFS_STORAGE dir exists in the system
+    if(stat(RFS_STORAGE, &sb) == -1)
+    {
+        LE_INFO("RFS storage dir does not exist, create it.");
+
+        if(mkdir(RFS_STORAGE, 0755) != 0)
+        {
+            LE_ERROR("Failed to create the dir %s", RFS_STORAGE);
+            exit(-1);
+        }
+
+        // go ahead creating the sub dir
+        if(mkdir(RFS_BACKUP_STORAGE, 0755) != 0)
+        {
+            LE_ERROR("Failed to create the dir %s", RFS_BACKUP_STORAGE);
+            exit(-1);
+        }
+    }
+    else if((sb.st_mode & S_IFMT) == S_IFDIR)
+    {
+        // assume the sub dir was created as well. do nothing
+        LE_INFO("RFS storage was found!");
+    }
+    else
+    {
+        // some other file objects. delete first
+        LE_ERROR("Delete the file object, then create the storage");
+
+        // try to delete it
+        unlink(RFS_STORAGE);
+
+        // Create the directory
+        if(mkdir(RFS_BACKUP_STORAGE, 0755) != 0)
+        {
+            LE_ERROR("Failed to create the dir %s", RFS_BACKUP_STORAGE);
+            exit(-1);
+        }
+
+        // go ahead creating the sub dir
+        if(mkdir(RFS_BACKUP_STORAGE, 0755) != 0)
+        {
+            LE_ERROR("Failed to create the dir %s", RFS_BACKUP_STORAGE);
+            exit(-1);
+        }
+    }
+
+    // Create an event Id for error event.
+    ErrorEventId = le_event_CreateId("ErrorEventId", sizeof(RFS_ErrorMsg_t));
+
+    // Register handler for error events.
+    le_event_AddHandler("ProcessBackupErrHandler",
+                            ErrorEventId,
+                            ProcessErrorHandler);
+}

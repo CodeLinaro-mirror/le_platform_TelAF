30f865ef4a766d85e9da9823b7f09401c5d4f91c
Samhitha Putta <quic_sputta@quicinc.com>
2024-03-22 21:02:17 +0530

    tafAudioSvc: Simulate VHAL return values for testing
    Change-Id: I8ece273aed5d2770bee2f6b4504dcc79e833d47c

--- apps/sample/vhal/audio/audioDriver/audioDrv.c
+++ apps/sample/vhal/audio/audioDriver/audioDrv.c
@@ -8,6 +8,11 @@
 #include "tafHalAudio.h"
 
 #define MAX_NODES 4
+#define CFG_NODE_TYPE "/data/le_fs/node%d/type"
+#define CFG_NODE_POWERSTATE "/data/le_fs/node%d/power"
+#define CFG_NODE_MUTESTATE "/data/le_fs/node%d/mute"
+#define CFG_NODE_GAIN "/data/le_fs/node%d/gain"
+#define CFG_NODE_EVENT "/data/le_fs/node%d/event"
 
 typedef struct
 {
@@ -28,6 +33,219 @@ le_mem_PoolRef_t NodeEventHandlerRefPool = NULL;
 le_dls_List_t NodeEventHandlerList;
 
 LE_MEM_DEFINE_STATIC_POOL(NodeEventHandlerRef, MAX_NODES, sizeof(NodeEventHandlerRef_t));
+
+static int GetNode_Type(uint8_t nodeId)
+{
+    FILE* file;
+    int type = -1;
+    char nodeTypePath[50];
+    snprintf(nodeTypePath, sizeof(nodeTypePath), CFG_NODE_TYPE, nodeId);
+    file = fopen(nodeTypePath, "r");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodeTypePath );
+        return -1;
+    }
+    while(fscanf(file, "%d", &type) != EOF)
+    {
+        LE_INFO("node %d type is %d", nodeId, type);
+    }
+
+    fclose(file);
+
+    return type;
+}
+
+static int GetNode_Power(uint8_t nodeId)
+{
+    FILE* file;
+    int power = -1;
+    char nodePowerPath[50];
+    snprintf(nodePowerPath, sizeof(nodePowerPath), CFG_NODE_POWERSTATE, nodeId);
+    file = fopen(nodePowerPath, "r");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodePowerPath );
+        return -1;
+    }
+    while(fscanf(file, "%d", &power) != EOF)
+    {
+        LE_INFO("node %d power state is %d", nodeId, power);
+    }
+
+    fclose(file);
+
+    return power;
+}
+
+static int SetNode_Power(uint8_t nodeId, int state)
+{
+    FILE* file;
+    char nodePowerPath[50];
+    snprintf(nodePowerPath, sizeof(nodePowerPath), CFG_NODE_POWERSTATE, nodeId);
+    file = fopen(nodePowerPath, "w");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodePowerPath );
+        return -1;
+    }
+    LE_INFO("state is %d", state);
+    fprintf(file, "%d", state);
+    fflush(file);
+    fclose(file);
+
+    return 0;
+}
+
+static int GetNode_Mute(uint8_t nodeId)
+{
+    FILE* file;
+    int isMute = -1;
+    char nodeMutePath[50];
+    snprintf(nodeMutePath, sizeof(nodeMutePath), CFG_NODE_MUTESTATE, nodeId);
+    file = fopen(nodeMutePath, "r");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodeMutePath );
+        return -1;
+    }
+    while(fscanf(file, "%d", &isMute) != EOF)
+    {
+        LE_INFO("node %d mute state is %d", nodeId, isMute);
+    }
+
+    fclose(file);
+
+    return isMute;
+}
+
+static int SetNode_Mute(uint8_t nodeId, int isMute)
+{
+    FILE* file;
+    char nodeMutePath[50];
+    snprintf(nodeMutePath, sizeof(nodeMutePath), CFG_NODE_MUTESTATE, nodeId);
+    file = fopen(nodeMutePath, "w");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodeMutePath );
+        return -1;
+    }
+    LE_INFO("isMute is %d", isMute);
+    fprintf(file, "%d", isMute);
+    fflush(file);
+    fclose(file);
+
+    return 0;
+}
+
+static int GetNode_Gain(uint8_t nodeId, hal_audio_direction_t direction, double *gain)
+{
+    FILE* file;
+    int integer;
+    double gainPerc;
+    char nodeGainPath[50];
+    snprintf(nodeGainPath, sizeof(nodeGainPath), CFG_NODE_GAIN, nodeId);
+    file = fopen(nodeGainPath, "r");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodeGainPath );
+        return -1;
+    }
+
+    while (fscanf(file, "%d:%lf", &integer, &gainPerc) != EOF) {
+        if(direction == (hal_audio_direction_t)integer) {
+            *gain = gainPerc;
+        }
+    }
+
+    fclose(file);
+
+    return 0;
+}
+
+static int SetNode_Gain(uint8_t nodeId, hal_audio_direction_t direction, double gain)
+{
+    FILE* file;
+    char nodeGainPath[50];
+    snprintf(nodeGainPath, sizeof(nodeGainPath), CFG_NODE_GAIN, nodeId);
+    file = fopen(nodeGainPath, "w");
+
+    if(file == NULL)
+    {
+        LE_WARN("type node %s doesn't exist", nodeGainPath );
+        return -1;
+    }
+    LE_INFO("gain is %f direction: %d", gain, direction);
+
+    fprintf(file, "%d:%f", direction, gain);
+    fflush(file);
+    fclose(file);
+
+    return 0;
+}
+
+static void* NodeEventThread
+(
+    void* contextPtr
+)
+{
+    int event = -1;
+    int prevEvent = -1;
+        NodeEventHandlerRef_t* nodeEventHandlerPtr = (NodeEventHandlerRef_t*)contextPtr;
+        int nodeId = nodeEventHandlerPtr->nodeId;
+        char nodeEventPath[50];
+        snprintf(nodeEventPath, sizeof(nodeEventPath), CFG_NODE_EVENT, nodeId);
+    LE_INFO("Start thread for %d node event", nodeId);
+
+    while(1)
+    {
+        FILE* file;
+
+        NodeEventHandlerRef_t* nodeEventHandlerPtr = (NodeEventHandlerRef_t*)contextPtr;
+        int nodeId = nodeEventHandlerPtr->nodeId;
+        char nodeEventPath[50];
+        snprintf(nodeEventPath, sizeof(nodeEventPath), CFG_NODE_EVENT, nodeId);
+
+        file = fopen(nodeEventPath, "r");
+
+        if(file != NULL)
+        {
+            while(fscanf(file, "%d", &event) != EOF)
+            {
+                if((event != -1) && (prevEvent != event)) {
+                    LE_INFO("config node %s is %d", nodeEventPath, event);
+                    prevEvent = event;
+                    le_dls_Link_t* linkHandlerPtr = NULL;
+                    linkHandlerPtr = le_dls_PeekTail(&NodeEventHandlerList);
+                    while(linkHandlerPtr)
+                    {
+                        NodeEventHandlerRef_t * handlerRefPtr = CONTAINER_OF(linkHandlerPtr,
+                                NodeEventHandlerRef_t, next);
+                        linkHandlerPtr = le_dls_PeekPrev(&NodeEventHandlerList, linkHandlerPtr);
+                        if(handlerRefPtr->nodeId == nodeId)
+                        {
+                            LE_INFO("NodeId registered received the event report it");
+                            NodeEvent_t nodeEvent;
+                            nodeEvent.nodeId = nodeId;
+                            nodeEvent.event = event;
+                            le_event_Report(handlerRefPtr->eventId, (void*)&nodeEvent,
+                                    sizeof(NodeEvent_t));
+                        }
+                    }
+                }
+                le_thread_Sleep(3);
+            }
+            fclose(file);
+        }
+    }
+    return 0;
+}
 
 static le_result_t taf_hal_CtlSetAudioStatus(bool status,
      uint32_t route, hal_audio_Mode_t mode)
@@ -55,7 +273,7 @@ static le_result_t taf_hal_GetNodeType
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId %d ",nodeId);
-    *nodeType = HAL_AUDIO_NODE_TYPE_CODEC;
+    *nodeType = (hal_audio_NodeType_t)GetNode_Type(nodeId);
     return LE_OK;
 }
 
@@ -78,7 +296,10 @@ static le_result_t taf_hal_SetNodePowerState
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId : %d powerState : %d", nodeId, state);
-    return LE_OK;
+    if( SetNode_Power(nodeId, (int)state) != -1) {
+        return LE_OK;
+    }
+    return LE_FAULT;
 }
 
 static le_result_t taf_hal_GetNodePowerState
@@ -89,7 +310,13 @@ static le_result_t taf_hal_GetNodePowerState
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId : %d", nodeId);
-    return LE_OK;
+    int pState = GetNode_Power(nodeId);
+    LE_DEBUG("pState is %d", pState);
+    if(pState != -1) {
+        *state = (hal_audio_PowerState_t)pState;
+        return LE_OK;
+    }
+    return LE_FAULT;
 }
 
 static le_result_t taf_hal_SetNodeMuteState
@@ -100,7 +327,10 @@ static le_result_t taf_hal_SetNodeMuteState
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId : %d mute : %s", nodeId, mute ? "true" : "false");
-    return LE_OK;
+    if( SetNode_Mute(nodeId, mute ? 1 : 0) != -1) {
+        return LE_OK;
+    }
+    return LE_FAULT;
 }
 
 static le_result_t taf_hal_GetNodeMuteState
@@ -111,7 +341,13 @@ static le_result_t taf_hal_GetNodeMuteState
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId : %d", nodeId);
-    return LE_OK;
+    int muteState = GetNode_Mute(nodeId);
+    if(muteState != -1)
+    {
+        *isMuted = muteState ? true : false;
+        return LE_OK;
+    }
+    return LE_FAULT;
 }
 
 static le_result_t taf_hal_SetNodeGain
@@ -123,6 +359,12 @@ static le_result_t taf_hal_SetNodeGain
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
     LE_DEBUG("nodeId : %d direction: %d gain: %f", nodeId, direction, gain);
+
+    int ret = SetNode_Gain(nodeId, direction, gain);
+    if(ret < 0){
+        return LE_FAULT;
+    }
+
     return LE_OK;
 }
 
@@ -134,7 +376,13 @@ static le_result_t taf_hal_GetNodeGain
 )
 {
     LE_DEBUG("AudioTestDrv: %s", __FUNCTION__);
-    LE_DEBUG("nodeId : %d direction: %d", nodeId, direction);
+    LE_DEBUG("nodeId : %d", nodeId);
+
+    int ret = GetNode_Gain(nodeId, direction, gain);
+    if(ret < 0){
+        return LE_FAULT;
+    }
+
     return LE_OK;
 }
 
@@ -203,7 +451,13 @@ static le_result_t taf_hal_AddNodeStateChangeHandler
             nodeHandlerRefPtr->eventId, NodeEventHandler, (void*)callback);
 
     le_dls_Queue(&NodeEventHandlerList, &nodeHandlerRefPtr->next);
-
+    //uint8_t* nodeIdPtr = &nodeId;
+    le_thread_Ref_t NodeEventThreadRef = le_thread_Create(
+                                                    "NodeEventThread",
+                                                    NodeEventThread,
+                                                    (void *)nodeHandlerRefPtr);
+    le_thread_Start(NodeEventThreadRef);
+    LE_INFO("Started thread for %d node event", nodeId);
     return LE_OK;
 }
 

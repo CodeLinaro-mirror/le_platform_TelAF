890b421a2455436ec629ae70d1fddada2fcbaa81
Suchand Ghosh <quic_suchghos@quicinc.com>
2024-05-08 13:40:56 +0530

    tafLocationSvc: Remove unnecessary logs which print frequently
    Change-Id: Ie1659e96e56f64438658da9d0be029ef487d0371

--- components/tafLocationSvc/tafGnssImpl.cpp
+++ components/tafLocationSvc/tafGnssImpl.cpp
@@ -545,12 +545,10 @@ void taf_Gnss::GnssPositionHandler
 void tafLocationListener::onDetailedEngineLocationUpdate(
       const std::vector<std::shared_ptr<telux::loc::ILocationInfoEx> > &locationEngineInfo) {
     auto &gnss = taf_Gnss::GetInstance();
-    LE_DEBUG("onDetailedEngineUpdate *sessionRef: %p", *clientSessionRef);
     taf_gnss_Client_t* clientRequestPtr = NULL;
     clientRequestPtr = gnss.DiscoverSessionRef(*clientSessionRef);
 
     if (NULL == clientRequestPtr) {
-        LE_DEBUG("onDetailedEngineUpdate did not find sessionRef: %p", *clientSessionRef);
         return;
     }
 
@@ -2007,12 +2005,8 @@ taf_gnss_Client_t* taf_Gnss::DiscoverSessionRef
             return NULL;
         }
 
-        LE_DEBUG("gnssPtr %p, gnssPtr->sessionRef %p, sessionRef %p",
-                 gnssPtr, gnssPtr->sessionRef, sessionRef);
-
         if (sessionRef == gnssPtr->sessionRef)
         {
-             LE_DEBUG("sessionRef %p found in Client session", sessionRef);
              return gnssPtr;
         }
         result = le_ref_NextNode(iterRef);

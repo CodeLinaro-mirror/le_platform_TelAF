0fd1b3711fe4a9a7566654a4db40a63021610f45
abalana <quic_abalana@quicinc.com>
2024-08-14 10:54:58 +0530

    tafMngdStorageSvc: Add support for config storage service
    - Add API to update configuration file
    - Add API to sync configuration file
    
    Change-Id: I1f4fd94b82bf9569fbfcf8d0398254ca6d874487

--- apps/tafMngdServices/Storage/Components/tafMngdStorageSvc/tafMngdStorageSvc.cpp
+++ apps/tafMngdServices/Storage/Components/tafMngdStorageSvc/tafMngdStorageSvc.cpp
@@ -190,6 +190,151 @@ le_result_t taf_mngdStorSec_DeleteData
     return mss.DeleteData(dataRef);
 }
 
+/**
+ * Copy the file from the paths configured in the MSS configuration JSON file to configuration
+ * storage and also checks the validity of file
+ */
+le_result_t taf_mngdStorCfg_UpdateFile
+(
+    void
+)
+{
+    auto &mss = tafMngdStorageSvc::GetInstance();
+
+    return mss.UpdateFile();
+}
+
+/**
+ * Replace the orignal configuration file with the updated file in config storage.
+ */
+le_result_t taf_mngdStorCfg_Sync
+(
+    void
+){
+    auto &mss = tafMngdStorageSvc::GetInstance();
+
+    return mss.Sync();
+}
+
+/**
+ * Revert configuration file with orignal version in config storage.
+ */
+le_result_t taf_mngdStorCfg_Rollback
+(
+    void
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the configuration storage reference.
+ */
+taf_mngdStorCfg_ConfigRef_t  taf_mngdStorCfg_GetRef
+(
+)
+{
+    return nullptr;
+}
+
+/**
+ * Get the major and minor version of the given configuration file reference.
+ */
+le_result_t taf_mngdStorCfg_GetVersion
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    uint32_t* MajorVersionPtr,
+    uint32_t* MinorVersionPtr,
+    uint32_t* PatchVersionPtr
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the value and data type of the node.
+ */
+le_result_t taf_mngdStorCfg_GetValue
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    taf_mngdStorCfg_NodeType_t* typePtr,
+    char* nodeValue,
+    size_t nodeValueSize
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the data type of the node.
+ */
+le_result_t taf_mngdStorCfg_GetType
+(
+   taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    taf_mngdStorCfg_NodeType_t* typePtr
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the value of the node with data type string.
+ */
+le_result_t taf_mngdStorCfg_GetString
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    char* nodeValue,
+    size_t nodeValueSize
+)
+{
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the value of the node with data type integer.
+ */
+le_result_t taf_mngdStorCfg_GetInt
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    int32_t* nodeValuePtr
+){
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the value of the node with data type float.
+ */
+le_result_t taf_mngdStorCfg_GetFloat
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    double* nodeValuePtr
+){
+    return LE_NOT_IMPLEMENTED;
+}
+
+/**
+ * Get the value of the node with data type boolean.
+ */
+le_result_t taf_mngdStorCfg_GetBool
+(
+    taf_mngdStorCfg_ConfigRef_t ConfigRef,
+    const char* LE_NONNULL groupName,
+    const char* LE_NONNULL nodeName,
+    int32_t* nodeValuePtr
+){
+    return LE_NOT_IMPLEMENTED;
+}
+
 COMPONENT_INIT
 {
     LE_INFO("tafMngdStorageSvc COMPONENT init...");

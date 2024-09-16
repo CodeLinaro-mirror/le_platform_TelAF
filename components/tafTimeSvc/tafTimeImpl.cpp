3f8bee102647eabb2780428bccc9a95278036dcb
Jaishika Joshi <quic_jaishika@quicinc.com>
2024-09-16 15:50:59 +0530

    tafTimeSvc: Improvement for fetching RTC time
    Change-Id: I7e740cf95495ccc7718e09c8ae76fe2c683163a2

--- components/tafTimeSvc/tafTimeImpl.cpp
+++ components/tafTimeSvc/tafTimeImpl.cpp
@@ -1832,6 +1832,10 @@ le_result_t taf_Time::CheckSourceTime
             {
                 result = GetAsyncRtcSetTimeStatus();
             }
+            else
+            {
+                result = GetRtcTime(timePtr);
+            }
             break;
 
         case TAF_TIME_SRC_NAME_GNSS:

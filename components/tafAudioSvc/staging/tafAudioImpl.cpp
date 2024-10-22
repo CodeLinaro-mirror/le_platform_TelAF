82056cd237aff00f6bd6a8175c05866620c4ef4c
Samhitha Putta <quic_sputta@quicinc.com>
2024-10-22 14:08:03 +0530

    tafAudioSvc: Add a workaround for getMute on Modem RX/TX
    Add workaround to avoid crash on platform audio server
    
    Change-Id: I37a43cb3a6217bf49dfb3e1b6b1d4cab0123e6c3

--- components/tafAudioSvc/staging/tafAudioImpl.cpp
+++ components/tafAudioSvc/staging/tafAudioImpl.cpp
@@ -3474,12 +3474,9 @@ le_result_t taf_Audio::GetMute
     } else if (streamPtr->interface == TAF_AUDIO_IF_DSP_BACKEND_MODEM_VOICE_RX)
     {
         LE_DEBUG("Get mute status of voice RX stream reference");
-        if(!mAudioVoiceStream) {
-            LE_DEBUG("Stream is not active, update the mute status of stream reference");
-            *isMute = streamPtr->isMute;
-            return LE_OK;
-        }
-        status = mAudioVoiceStream->getMute(StreamDirection::RX,
+        *isMute = streamPtr->isMute;
+        return LE_OK;
+        /*status = mAudioVoiceStream->getMute(StreamDirection::RX,
                 [&p, &responseStatus, &muteObj, this](StreamMute mute, ErrorCode error) {
             if (error == ErrorCode::SUCCESS) {
                 responseStatus = telux::common::Status::SUCCESS;
@@ -3490,16 +3487,13 @@ le_result_t taf_Audio::GetMute
                 responseStatus = telux::common::Status::FAILED;
                 p.set_value(false);
             }
-        });
+        });*/
     } else if (streamPtr->interface == TAF_AUDIO_IF_DSP_BACKEND_MODEM_VOICE_TX)
     {
         LE_DEBUG("Get mute status of voice TX stream reference");
-        if(!mAudioVoiceStream) {
-            LE_DEBUG("Stream is not active, update the mute status of stream reference");
-            *isMute = streamPtr->isMute;
-            return LE_OK;
-        }
-        status = mAudioVoiceStream->getMute(StreamDirection::TX,
+        *isMute = streamPtr->isMute;
+        return LE_OK;
+        /*status = mAudioVoiceStream->getMute(StreamDirection::TX,
                 [&p, &responseStatus, &muteObj, this](StreamMute mute, ErrorCode error) {
             if (error == ErrorCode::SUCCESS) {
                 responseStatus = telux::common::Status::SUCCESS;
@@ -3510,7 +3504,7 @@ le_result_t taf_Audio::GetMute
                 responseStatus = telux::common::Status::FAILED;
                 p.set_value(false);
             }
-        });
+        });*/
     } else
     {
         LE_ERROR("Invalid stream reference");

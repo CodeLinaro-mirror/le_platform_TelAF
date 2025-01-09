6c4780d5b2c5055e92d223db92b8b98c50641f31
csurendr <quic_csurendr@quicinc.com>
2025-01-09 15:05:08 +0530

    tafDiagGen: Add sample schemas for V1
    Change-Id: I759a0893bf10776262870cc9ac2ff84490ce1121

--- apps/tools/tafDiagGen/V1/samples/extended/exchecker.py
+++ apps/tools/tafDiagGen/V1/samples/extended/exchecker.py
@@ -28,13 +28,6 @@ P_CAMEL_CASE = r'(^[a-z]|[A-Z0-9])[a-zA-Z]*'  # Camel case-sensitive pattern
 P_SOURCE_CODE_FUNCTION_CASE = r'[a-z]+(?:[A-Z][a-z0-9]+)+'  # Source code function case-sensitive pattern
 P_YAML_RANGE = r'\(\s?\'?' + PATTERN_HEXA_YAML_FORMAT + r'\'?..\'?' + PATTERN_HEXA_YAML_FORMAT + r'\'?\s?\)'
 P_SW_CONTEXT_PATH = r'/?' + P_CAMEL_CASE + r'(?:/' + P_SOURCE_CODE_FUNCTION_CASE + r')+'
-P_PART_RENAULT_REFERENCE = r'^[\dA-Z]{9}R$'
-P_MANUFACTURER_SPARE_PART_NUMBER_F187 = r'^[\dA-Z]{9}R$'
-P_MANUFACTURER_ECU_SOFTWARE_NUMBER_F188 = r'^[\dA-Z]{9}R$'
-P_MANUFACTURER_ECU_HARDWARE_NUMBER_F191 = r'^[\dA-Z]{9}R$'
-P_SUPPLIER_ECU_SOFTWARE_NUMBER_F194 = r'^[_.\dA-Z]+\.\d+\.\d{2}\.\d{2}\.\d{2}(M|S)$'
-P_SUPPLIER_ECU_SOFTWARE_VERSION_NUMBER_F195 = r'^[\dA-Z]{9}R$'
-P_SW_BASELINE = r'^[_.\dA-Z]+$'
 
 # https://semver.org/
 PATTERN_SEMVER_FORMAT = r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
@@ -176,70 +169,6 @@ def semver__(required = True):
     return semver_x
 
 @fn_origin
-def partRenaultRef__(required = True):
-    def partRenaultRef_x(pair):
-        if isinstance(pair.node.value, str) and re.match(P_PART_RENAULT_REFERENCE, pair.node.value):
-            raise CommonCheckerError(pair)
-    partRenaultRef_x.__name__ = "partRenaultRef"
-    partRenaultRef_x.required = required
-    return partRenaultRef_x
-
-@fn_origin
-def VehicleManufacturerSparePartNumber__(required = True):
-    def VehicleManufacturerSparePartNumber_x(pair):
-
-        if isinstance(pair.node.value, str) and re.match(P_MANUFACTURER_SPARE_PART_NUMBER_F187, pair.node.value):
-            raise CommonCheckerError(pair)
-    VehicleManufacturerSparePartNumber_x.__name__ = "VehicleManufacturerSparePartNumber"
-    VehicleManufacturerSparePartNumber_x.required = required
-    return VehicleManufacturerSparePartNumber_x
-
-@fn_origin
-def VehicleManufacturerECUHardwareNumber__(required = True):
-    def VehicleManufacturerECUHardwareNumber_x(pair):
-        if not ( isinstance(pair.node.value, str) and re.match(P_MANUFACTURER_ECU_HARDWARE_NUMBER_F191, pair.node.value) ):
-            raise CommonCheckerError(pair)
-    VehicleManufacturerECUHardwareNumber_x.__name__ = "VehicleManufacturerECUHardwareNumber"
-    VehicleManufacturerECUHardwareNumber_x.required = required
-    return VehicleManufacturerECUHardwareNumber_x
-
-@fn_origin
-def VehicleManufacturerECUSoftwareNumber__(required = True):
-    def VehicleManufacturerECUSoftwareNumber_x(pair):
-        if not ( isinstance(pair.node.value, str) and re.match(P_MANUFACTURER_ECU_SOFTWARE_NUMBER_F188, pair.node.value) ):
-            raise CommonCheckerError(pair)
-    VehicleManufacturerECUSoftwareNumber_x.__name__ = "VehicleManufacturerECUSoftwareNumber"
-    VehicleManufacturerECUSoftwareNumber_x.required = required
-    return VehicleManufacturerECUSoftwareNumber_x
-
-@fn_origin
-def SystemSupplierECUSoftwareNumber__(required = True):
-    def SystemSupplierECUSoftwareNumber_x(pair):
-        if not ( isinstance(pair.node.value, str) and re.match(P_SUPPLIER_ECU_SOFTWARE_NUMBER_F194, pair.node.value) ) or pair.node.value is not None:
-            raise CommonCheckerError(pair)
-    SystemSupplierECUSoftwareNumber_x.__name__ = "SystemSupplierECUSoftwareNumber"
-    SystemSupplierECUSoftwareNumber_x.required = required
-    return SystemSupplierECUSoftwareNumber_x
-
-@fn_origin
-def SystemSupplierECUSoftwareVersionNumber__(required = True):
-    def SystemSupplierECUSoftwareVersionNumber_x(pair):
-        if not ( isinstance(pair.node.value, str) and re.match(P_SUPPLIER_ECU_SOFTWARE_VERSION_NUMBER_F195, pair.node.value) ) or pair.node.value is not None:
-            raise CommonCheckerError(pair)
-    SystemSupplierECUSoftwareVersionNumber_x.__name__ = "SystemSupplierECUSoftwareNumber"
-    SystemSupplierECUSoftwareVersionNumber_x.required = required
-    return SystemSupplierECUSoftwareVersionNumber_x
-
-@fn_origin
-def BaselineSW__(required = True):
-    def BaselineSW_x(pair):
-        if not ( isinstance(pair.node.value, str) and re.match(P_SW_BASELINE, pair.node.value) ) or pair.node.value is not None:
-            raise CommonCheckerError(pair)
-    BaselineSW_x.__name__ = "BaselineSW"
-    BaselineSW_x.required = required
-    return BaselineSW_x
-
-@fn_origin
 def ExecutionAuthorizationPattern__(required = True):
     def ExecutionAuthorizationPattern_x(pair):
         pass
@@ -273,13 +202,6 @@ class ExChecker(Checker):
             'hexa2bytes': hexa2bytes__,
             'hexa1byte' : hexa1byte__,
             'semver': semver__,
-            'partRenaultRef': partRenaultRef__,
-            'VehicleManufacturerSparePartNumber': VehicleManufacturerSparePartNumber__,
-            'VehicleManufacturerECUHardwareNumber': VehicleManufacturerECUHardwareNumber__,
-            'VehicleManufacturerECUSoftwareNumber': VehicleManufacturerECUSoftwareNumber__,
-            'SystemSupplierECUSoftwareNumber' : SystemSupplierECUSoftwareNumber__,
-            'SystemSupplierECUSoftwareVersionNumber': SystemSupplierECUSoftwareVersionNumber__,
-            'BaselineSW' : BaselineSW__,
             'ExecutionAuthorizationPattern' : ExecutionAuthorizationPattern__,
             'CapsNDash': CapsNDash__,
         }

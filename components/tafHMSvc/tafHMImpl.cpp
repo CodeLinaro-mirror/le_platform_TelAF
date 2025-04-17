ac209e19c3d33ef94fbe358725c183ad7a94a39c
Sumit Kumar <quic_sumkum@quicinc.com>
2025-04-17 16:14:20 +0530

    tafHMSvc: Get the reboot sub-reason
    Change-Id: I84feef695020da41dd78fc523945659cd37fdefe

--- components/tafHMSvc/tafHMImpl.cpp
+++ components/tafHMSvc/tafHMImpl.cpp
@@ -1418,6 +1418,35 @@ std::string BootReasonToString(taf_hms_SubReason_t reason)
 
 //--------------------------------------------------------------------------------------------------
 /**
+ ** Reads the boot sub-reason from a file.
+ **
+ */
+//--------------------------------------------------------------------------------------------------
+le_result_t taf_Hms::ReadSubReason
+(
+    const std::string& filePath,
+    char* subReasonStr,
+    size_t subReasonSize
+)
+{
+    std::string tmpStr;
+    std::ifstream file(filePath);
+    if (!file.is_open())
+    {
+        LE_ERROR("Failed to open file: %s", filePath.c_str());
+        return LE_NOT_FOUND;
+    }
+
+    std::getline(file, tmpStr);
+    file.close();
+
+    LE_DEBUG("Reboot sub-reason string: %s",tmpStr.c_str());
+
+    return le_utf8_Copy(subReasonStr, tmpStr.c_str(), subReasonSize, NULL);
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
  ** Reads the boot reason for a file.
  **
  */
@@ -1426,7 +1455,8 @@ le_result_t taf_Hms::ReadReason
 (
     const std::string& filePath,
     taf_hms_SubReason_t* reason,
-    char* reasonStr
+    char* reasonStr,
+    size_t reasonSize
 )
 {
     std::string tmpStr;
@@ -1451,8 +1481,15 @@ le_result_t taf_Hms::ReadReason
     {
         *reason = TAF_HMS_BOOTREASON_UNKNOWN;
     }
-    le_utf8_Copy(reasonStr, tmpStr.c_str(), TAF_HMS_MAX_RESET_LEN, NULL);
-    return LE_OK;
+
+    if (*reason == TAF_HMS_BOOTREASON_ADMIN_TRIGGER)
+    {
+        return  ReadSubReason(TAF_HMS_BOOT_SUB_REASON_PATH, reasonStr, reasonSize);
+    }
+    else
+    {
+        return le_utf8_Copy(reasonStr, tmpStr.c_str(), reasonSize, NULL);
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -1481,7 +1518,8 @@ le_result_t taf_Hms::GetResetInformation
         LE_ERROR("resetSpecificInfoStrSize is not correct: %zu", resetSpecificInfoStrSize);
         return LE_BAD_PARAMETER;
     }
-    res = ReadReason(TAF_HMS_BOOT_REASON_PATH, &subReason, resetSpecificInfoStrPtr);
+    res = ReadReason(TAF_HMS_BOOT_REASON_PATH, &subReason, resetSpecificInfoStrPtr,
+        resetSpecificInfoStrSize);
     if (LE_OK != res)
     {
         return res;
@@ -1513,6 +1551,9 @@ le_result_t taf_Hms::GetResetInformation
             break;
 
         case TAF_HMS_BOOTREASON_ADMIN_TRIGGER:
+            reason = TAF_HMS_RESET_CRASH;
+            break;
+
         case TAF_HMS_BOOTREASON_UNKNOWN:
         default:
             reason = TAF_HMS_RESET_UNKNOWN;

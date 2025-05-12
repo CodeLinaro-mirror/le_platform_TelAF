6f645d30029f37ff29873114c62f27c410beda38
amanokar <amanokar@qti.qualcomm.com>
2025-05-12 17:48:54 +0530

    tafSimCardSvc: Change license markings based on new guidelines
    Change-Id: I34730ca44c26d1315107ef0e68e96fcd24edbed7

--- components/tafSimCardSvc/tafSimCard.hpp
+++ components/tafSimCardSvc/tafSimCard.hpp
@@ -28,10 +28,11 @@
  */
 
 /*
- *  Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- *  Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *  SPDX-License-Identifier: BSD-3-Clause-Clear
- */
+*/
+
 
 
 #include "legato.h"

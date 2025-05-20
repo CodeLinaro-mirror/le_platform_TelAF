bcdb246cd19c0c28ff3c633d641b31ab0434f226
Samhitha Putta <sputta@qti.qualcomm.com>
2025-05-20 12:38:15 +0530

    telaf: Strip telaf-pa, telaf-noship, telaf-prop binaries
    Change-Id: I1e5685d3e8242270cae0292e51a88644df9ffea2

--- mkimg.sh
+++ mkimg.sh
@@ -1,36 +1,5 @@
-#!/bin/sh
-
-# Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted (subject to the limitations in the
-# disclaimer below) provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-#     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-#       contributors may be used to endorse or promote products derived
-#       from this software without specific prior written permission.
-#
-# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-# GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-# HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
 
 export SELINUX_FILE_CONTEXTS=${TELAF_ROOT}/security/selinux/sepolicy/files/file_contexts
 TARGET=$1
@@ -63,6 +32,8 @@ do
     echo "*** try to use ${base_name} replace telaf-prop stub library ***"
     full_name_telaf=`find ${TARGET_STAGE_DIR} -type f -name ${base_name}`
     if [ -n "${full_name_telaf}" ]; then
+        echo "stripping ${full_name_prop}"
+        ${STRIP} --strip-unneeded ${full_name_prop}
         cp -rf ${full_name_prop} ${full_name_telaf}
     fi
 done
@@ -76,21 +47,25 @@ do
     if [ -n "${full_name_telaf}" ]; then
         for each_lib_name in ${full_name_telaf}
         do
+            echo "stripping ${full_name_noship}"
+            ${STRIP} --strip-unneeded ${full_name_noship}
             cp -rf ${full_name_noship} ${each_lib_name}
         done
     fi
 done
 
 echo "*** searching path: ${PA_BUILD_DIR} ***"
-for full_name_noship in `find ${PA_BUILD_DIR} -maxdepth 1 -type f -name "*.so"`
+for full_name_pa in `find ${PA_BUILD_DIR} -maxdepth 1 -type f -name "*.so"`
 do
-    base_name=`basename ${full_name_noship}`
+    base_name=`basename ${full_name_pa}`
     echo "*** try to use ${base_name} replace telaf-pa stub library ***"
     full_name_telaf=`find ${TARGET_STAGE_DIR} -type f -name ${base_name}`
     if [ -n "${full_name_telaf}" ]; then
         for each_lib_name in ${full_name_telaf}
         do
-            cp -rf ${full_name_noship} ${each_lib_name}
+            echo "stripping ${full_name_pa}"
+            ${STRIP} --strip-unneeded ${full_name_pa}
+            cp -rf ${full_name_pa} ${each_lib_name}
         done
     fi
 done

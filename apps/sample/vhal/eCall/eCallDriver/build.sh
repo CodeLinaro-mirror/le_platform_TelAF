bb422e7a7cf84d5ef79208468173144d51bdfe3f
Andrew Zhao <zhax@qti.qualcomm.com>
2025-10-23 15:46:35 +0800

    telaf: Enable c++17 for vhal and plugin build
    Change-Id: I1a454143672aec56de01601af80ca4e365976721
    Signed-off-by: Andrew Zhao <zhax@qti.qualcomm.com>

--- apps/sample/vhal/eCall/eCallDriver/build.sh
+++ apps/sample/vhal/eCall/eCallDriver/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 echo "Enter target (sa515m or sa525m):"
@@ -13,7 +13,7 @@ then
         ${MKTOOLS_X_C_FLAGS} \
         -X "-fPIC" \
         -X "-shared" \
-        -X "-std=c++11" \
+        -X "-std=c++17" \
         -o sa525m/eCallVHalDrv.so \
         .
 
@@ -25,8 +25,8 @@ then
         ${MKTOOLS_X_C_FLAGS} \
         -X "-fPIC" \
         -X "-shared" \
-        -X "-std=c++11" \
-        -o sa515m/gpioVHalDrv.so \
+        -X "-std=c++17" \
+        -o sa515m/eCallVHalDrv.so \
         .
 
 else

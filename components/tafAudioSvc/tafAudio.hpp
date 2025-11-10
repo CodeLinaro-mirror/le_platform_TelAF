22d7c3a7c4df37df8fb41734fb89689f71d1bbe3
Samhitha Putta <sputta@qti.qualcomm.com>
2025-11-10 09:55:59 +0530

    tafAudioSvc: Handle SIGTERM gracefully
    Fix crash issue when service restarts during capture scenario
    
    Change-Id: Ic6b4e5585476d901e3ea548a0bb7626ae88b90e1

--- components/tafAudioSvc/tafAudio.hpp
+++ components/tafAudioSvc/tafAudio.hpp
@@ -372,6 +372,7 @@ class taf_Audio : public ITafSvc
         bool isRxRecMuteSet = false;
         bool mIsLoopbackActive = false;
         le_sem_Ref_t mPlayCompletedSemRef;
+        le_thread_Ref_t bufferHandlingThreadRef;
         le_dls_List_t  EventIdList = LE_DLS_LIST_INIT;
         taf_audio_StreamRef_t mDtmfAudioRef = NULL;
         taf_audio_Stream_t* mTxRecStreamPtr = NULL;
@@ -508,6 +509,7 @@ class taf_Audio : public ITafSvc
         void RecBufferHandler(taf_audio_Stream_t* streamPtr);
         void AdvertiseAndRegisterHandler();
         void StartMpmsRetryTimer();
+        void CleanUpBeforeExit();
 
         static void ClientSessionCloseEventHandler( le_msg_SessionRef_t sessionRef,
                             void* contextPtr);
@@ -531,5 +533,6 @@ class taf_Audio : public ITafSvc
         static void MpmsConnectHandler(le_timer_Ref_t timerRef);
         static void MpmsDelayHandler(le_timer_Ref_t timerRef);
         static void MpmsDisconnectHandler(void* contextPtr);
+        static void TafSigTermEventHandler(int tafSigNum);
 };
 }

08c450770e00496c968ce6b672fe4b5602b28f64
Yan Kong <xiankong@qti.qualcomm.com>
2025-12-02 21:42:39 +0800

    telaf: Remove unused TELUX_WLAN_SUPPORTED macro
    The build macro TELUX_WLAN_SUPPORTED is no longer used since TelAF
    has been decoupled from TELUX. Remove it to clean up config.mk.
    
    Change-Id: Id251e324211a26e38782a5c446acfc76abb401ec

--- config.mk
+++ config.mk
@@ -1,10 +1,4 @@
 
-ifneq ($(wildcard $(PKG_CONFIG_SYSROOT_DIR)/usr/lib/libtelux_wlan.so),)
-    export TELUX_WLAN_SUPPORTED := y
-else
-    export TELUX_WLAN_SUPPORTED := n
-endif
-
 TELUX_PC := $(PKG_CONFIG_SYSROOT_DIR)/usr/lib/pkgconfig/telux.pc
 TELUX_VERSION := $(shell find $(TELUX_PC) 2> /dev/null | xargs -r grep '^Version:' | awk '{print $$2}')
 ifneq ($(TELUX_VERSION),)
